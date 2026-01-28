7e7be7f3004585a42d85e18ee0e3ff7f09f878f4
roberthouse38 <monkmalone14@gmail.com>
2026-01-28 23:02:36 +0700

    added photo feature

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const captureBtn = document.getElementById("captureBtn");
 const canvas = document.getElementById("canvas");
 const photo = document.getElementById("photo");
 const ctx = canvas.getContext("2d");
-
-
+const photoGrid = document.getElementById("photoGrid");
+let currentFilter = "none";
+let photoCount = 0;
 
 // Akses Kamera
 navigator.mediaDevices.getUserMedia( {video: true} )
@@ -18,9 +19,36 @@ navigator.mediaDevices.getUserMedia( {video: true} )
 
 // Ambil Foto
 captureBtn.addEventListener("click", () => {
+    if (photoCount >= 4) {
+        photoGrid.innerHTML = "";
+        photoCount = 0;
+    }
+
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
+    if (currentFilter === "grayscale") {
+        ctx.filter = "grayscale(100%)";
+    } else if (currentFilter === "sepia") {
+        ctx.filter = "sepia(100%)";
+    } else {
+        ctx.filter = "none";
+    }
+
     ctx.drawImage(video, 0, 0);
-    photo.src = canvas.toDataURL("image/png");
+    ctx.filter = "none";
+
+    const img = document.createElement("img");
+    img.src = canvas.toDataURL("image/png");
+
+    photoGrid.appendChild(img);
+    photoCount++;
+});
+
+
+// Filter foto
+document.querySelectorAll("[data-filter]").forEach(btn => {
+    btn.addEventListener("click", ()=> {
+        currentFilter = btn.dataset.filter;
+    });
 });
